7f5973526fe60bfd79e40f7992d695aa9166cbe1
suiboxi <suiboxi@qq.com>
2025-01-26 22:06:01 +0800

    Add files via upload
    这是一个可以将c语言中常见的一些拼写错误纠正对的头文件

---
+++ shabi.h
@@ -0,0 +1,20 @@
+#ifndef SB_H
+#define SB_H
+
+#define retun return
+#define retrun return
+#define retrn return
+
+#define incude include
+#define inclde include
+#define inlcude include
+
+#define mian main
+#define maim main
+#define mnai main
+
+#define stdio <stdio.h>
+
+#define pritnf printf
+
+#endif 
